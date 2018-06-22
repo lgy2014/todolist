897fe3223710f190de9d412985d9cab114ba70b8
lgy <lgy@qq.com>
2018-06-22 16:17:15 +0800

    1

--- pyscript/001发布July到142.py
+++ pyscript/001发布July到142.py
@@ -2,27 +2,12 @@ import os
 import shutil
 import util
 
-
-def mycodysimple(dir1,dir2):
-    ignores=["AppOffLine","DownLoad","Test","UploadFile","EDI","TemplateFiles","Images"]
-    list=os.listdir(dir1)
-    for i in list:
-        if os.path.isdir(os.path.join(dir1,i)):
-            if i in ignores:
-                continue
-            else:
-                target=os.path.join(dir2,i)
-                util.mycody(os.path.join(dir1,i),target)
+# ignore_lms=("AppOffLine","DownLoad","Test","UploadFile","EDI","TemplateFiles","Images",".pdb","AppSettings.config","Blank.aspx","default.aspx","Error.aspx","FrameInfo.aspx","GetConfigurableId.ashx","Global.asax","HomePage.aspx","Index.aspx","log4net.config","packages.config","Web.config","注释.txt")
+# july1=u"D:\\publish\\LMSWeb"
+# julytest=u"D:\\publish\\test"
+# util.mycody(july1,julytest,ignore=ignore_lms)
 
 # july 发布到 142 脚本
+ignore_lms=("bin","Scripts","Styles","TemplateFiles","AppOffLine","DownLoad","Test","UploadFile","EDI","TemplateFiles","Images",".pdb","AppSettings.config","Blank.aspx","default.aspx","Error.aspx","FrameInfo.aspx","GetConfigurableId.ashx","Global.asax","HomePage.aspx","Index.aspx","log4net.config","packages.config","Web.config","注释.txt")
 july1=u"D:\\publish\\LMSWeb"
 july2=u"\\\\10.99.110.142\\d$\\LMS\\LMSJuly_21"
-
-# mycodysimple(july1,july2)
-
-# ignore_lms=("AppOffLine","DownLoad","Test","UploadFile","EDI","TemplateFiles","Images",".pdb","AppSettings.config","Blank.aspx","default.aspx","Error.aspx","FrameInfo.aspx","GetConfigurableId.ashx","Global.asax","HomePage.aspx","Index.aspx","log4net.config","packages.config","Web.config","注释.txt")
-ignore_lms=("bin","Scripts","Styles","TemplateFiles","AppOffLine","DownLoad","Test","UploadFile","EDI","TemplateFiles","Images",".pdb","AppSettings.config","Blank.aspx","default.aspx","Error.aspx","FrameInfo.aspx","GetConfigurableId.ashx","Global.asax","HomePage.aspx","Index.aspx","log4net.config","packages.config","Web.config","注释.txt")
-
-
-julytest=u"D:\\publish\\test"
-util.mycody(july1,julytest,ignore=ignore_lms)
