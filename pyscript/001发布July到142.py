0e8369664c06f4bdae960d243d33c254acb47f4d
lgy <lgy@qq.com>
2018-06-28 18:34:57 +0800

    1

--- pyscript/001发布July到142.py
+++ pyscript/001发布July到142.py
@@ -2,19 +2,8 @@ import os
 import shutil
 import util
 
-# ignore_lms=("bin","Scripts","Styles","TemplateFiles","AppOffLine","DownLoad","Test","UploadFile","EDI","TemplateFiles","Images",".pdb","AppSettings.config","Blank.aspx","default.aspx","Error.aspx","FrameInfo.aspx","GetConfigurableId.ashx","Global.asax","HomePage.aspx","Index.aspx","log4net.config","packages.config","Web.config","注释.txt")
-# july1=u"D:\\publish\\LMSWeb"
-# julytest=u"D:\\publish\\test"
-# util.mycody(july1,julytest,ignore=ignore_lms)
-
-
-
 # july 发布到 142 脚本
-ignore_lms=(".pdb","TemplateFiles","AppOffLine","DownLoad","Test","UploadFile","EDI","Images","jquery1.9.1","jquery-tmpl-master","kindeditor-3.4","layui","My97DatePicker","artdialog","SwfUpload","uploadify","ymPrompt","AppSettings.config","Blank.aspx","default.aspx","Error.aspx","FrameInfo.aspx","GetConfigurableId.ashx","Global.asax","HomePage.aspx","Index.aspx","log4net.config","packages.config","Web.config","注释.txt")
-
-# ignore_lms = ignore_lms +("bin")
-# ignore_lms = ignore_lms +("Styles")
-# ignore_lms = ignore_lms +("Scripts")
+ignore_lms=(".pdb","TemplateFiles","Styles","AppOffLine","DownLoad","Test","UploadFile","EDI","Images","jquery1.9.1","jquery-tmpl-master","kindeditor-3.4","layui","My97DatePicker","artdialog","SwfUpload","uploadify","ymPrompt","AppSettings.config","Blank.aspx","default.aspx","Error.aspx","FrameInfo.aspx","GetConfigurableId.ashx","Global.asax","HomePage.aspx","Index.aspx","log4net.config","packages.config","Web.config","注释.txt")
 
 july1=u"D:\\publish\\LMSWeb"
 july2=u"\\\\10.99.110.142\\d$\\LMS\\LMSJuly_21"
