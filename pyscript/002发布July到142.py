7a56e2fd7e26dfdcee2a7c403bb02716a1e12a5b
lgy <lgy@qq.com>
2018-07-02 16:14:34 +0800

    1

--- pyscript/002发布July到142.py
+++ pyscript/002发布July到142.py
@@ -3,7 +3,8 @@ import util
 # 过滤掉常规文件，加快发布速度
 ignore=['/AppOffLine','/*.config','/*.aspx','/*.ashx','/*.asax','/*.txt','/Styles','/DownLoad','/Test','*.zip','*.rar','*.pdb','temp*','images','bootstrap*','jquery*','artdialog*','echarts*','kindeditor*','layui','my97datepic*','swfupload*','ymprompt*','uploadfile*','npoi*','lenovo*.dll','microsoft*','QuotationModel','ContractModel']
 
-ignore=['*.pdb','/test','/AppOffLine','/*.config','/*.aspx','/*.ashx','/*.asax','/*.txt']
+# full
+# ignore=['*.pdb','/test','/AppOffLine','/*.config','/*.aspx','/*.ashx','/*.asax','/*.txt']
 
 path="D:\\publish\\LMSWeb"
 path2="D:\\publish\\test"
