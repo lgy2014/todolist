ed2969184cc7f6b4e8d7f7e2bfd1d2972ab561dc
lgy <lgy@qq.com>
2018-07-31 17:02:20 +0800

    todo 功能完成

---
+++ iview-admin-master/src/view/todo/item.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    正在进行
+<ul>
+    <li 
+    v-for="(todo,index) in todos"
+    v-if="!todo.isCompleted" 
+    v-bind:key="todo.id"
+    :index="index"
+><Checkbox v-model="todo.isCompleted">{{todo.text}}</Checkbox><Button type="ghost" size="small" @click.native="todos.splice(index,1)" shape="circle" icon="android-remove"></Button></li>
+</ul>
+已完成
+<ul>
+    <li 
+    v-for="(todo,index) in todos"
+    v-if="todo.isCompleted" 
+    v-bind:key="todo.id"
+    :index="index"
+><Checkbox v-model="todo.isCompleted">{{todo.text}}</Checkbox><Button type="ghost" size="small" @click.native="todos.splice(index,1)" shape="circle" icon="android-remove"></Button></li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: Array
+  },
+  methods: {
+  }
+};
+</script>
+<style>
+ul {
+  list-style: none;
+}
+</style>
--- iview-admin-master/src/view/todo/todo.vue
+++ iview-admin-master/src/view/todo/todo.vue
@@ -1,8 +1,48 @@
 <template>
-
+<div>
+    <Row>
+        <Col span="24">
+        <h3>todos</h3>
+            <Input v-model="value" @keyup.enter.native="addItem" size="large" placeholder="todo something ..." style="width: 300px"></Input>
+            <Item v-bind:todos="todos"></Item>
+            <Button type="success" @click.native="clearAll">清除全部</Button>
+            <div><span>Copyright © 2018 liugy</span></div>
+        </Col>
+    </Row>
+</div>
 </template>
 <script>
-export default {
+import Item from "./item.vue";
+import Store from "./store"
 
-}
+export default {
+  data() {
+    return {
+      value: "",
+      index:0,
+      todos: Store.fetch()
+    };
+  },
+  watch:{
+      todos:{
+          handler:function(){
+              Store.save(this.todos);
+          },
+          deep:true
+      }
+  },
+  components: {
+    Item
+  },
+  methods: {
+    addItem: function(event) {
+      var item = { id:this.index++,text: this.value,isCompleted:false };
+      this.todos.unshift(item);
+      this.value = "";
+    },
+    clearAll:function(){
+        this.todos.splice(0,this.index);
+    }
+  }
+};
 </script>
