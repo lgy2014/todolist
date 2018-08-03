bf098315da0167b091c749c28ff6f7e426b11dec
lgy <lgy@qq.com>
2018-08-03 13:47:12 +0800

    1

--- iview-admin-master/src/view/todo/item.vue
+++ iview-admin-master/src/view/todo/item.vue
@@ -3,11 +3,15 @@
   <br />
     <h2>正在进行</h2>
 <ul>
+  <draggable v-model="todos" :options="{group:'todo'}" @start="drag=true" @end="drag=false">
     <li 
     v-for="(todo,index) in todos"
     v-if="!todo.isCompleted" 
     :index="index"
+    :key="todo.text"
+    v-model="todolist"
 ><Checkbox v-model="todo.isCompleted" size="large">{{todo.text}}</Checkbox><Button type="ghost" size="small" @click.native="todos.splice(index,1)" shape="circle" icon="android-remove"></Button></li>
+</draggable>
 </ul>
 <br />
     <h2>已完成</h2>
@@ -22,11 +26,20 @@
 </template>
 
 <script>
+import draggable from "vuedraggable";
+
 export default {
+  data() {
+    return {
+      todolist: this.todos
+    };
+  },
   props: {
     todos: Array
   },
-  methods: {
+  methods: {},
+  components: {
+    draggable
   }
 };
 </script>
@@ -35,9 +48,9 @@ ul {
   list-style: none;
 }
 
-label{
-  width:90%;
-  border:1px solid #dddee1;
+label {
+  width: 90%;
+  border: 1px solid #dddee1;
   font-size: 18px;
 }
 </style>
