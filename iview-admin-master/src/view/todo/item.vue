51158df1dfe59f8abbdd697a523b6efc35a9fe77
lgy <lgy@qq.com>
2018-08-03 13:48:53 +0800

    去掉拖动排序

--- iview-admin-master/src/view/todo/item.vue
+++ iview-admin-master/src/view/todo/item.vue
@@ -3,15 +3,12 @@
   <br />
     <h2>正在进行</h2>
 <ul>
-  <draggable v-model="todos" :options="{group:'todo'}" @start="drag=true" @end="drag=false">
     <li 
     v-for="(todo,index) in todos"
     v-if="!todo.isCompleted" 
     :index="index"
     :key="todo.text"
-    v-model="todolist"
 ><Checkbox v-model="todo.isCompleted" size="large">{{todo.text}}</Checkbox><Button type="ghost" size="small" @click.native="todos.splice(index,1)" shape="circle" icon="android-remove"></Button></li>
-</draggable>
 </ul>
 <br />
     <h2>已完成</h2>
@@ -26,20 +23,12 @@
 </template>
 
 <script>
-import draggable from "vuedraggable";
-
 export default {
-  data() {
-    return {
-      todolist: this.todos
-    };
-  },
   props: {
     todos: Array
   },
   methods: {},
   components: {
-    draggable
   }
 };
 </script>
