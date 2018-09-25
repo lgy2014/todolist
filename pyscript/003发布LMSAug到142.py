bcc2ba7463af619f15e12c26e5f20390f8fd4dd0
lgy <lgy@qq.com>
2018-09-25 12:32:55 +0800

    1

--- pyscript/003发布LMSAug到142.py
+++ pyscript/003发布LMSAug到142.py
@@ -21,7 +21,7 @@ def FormatTime(second):
 
 start_time = time.time()
 # 过滤掉常规文件，加快发布速度
-ignore = ['/AppOffLine', '/*.config', '/*.aspx', '/*.ashx', '/*.asax', '/*.txt', '/Styles', '/DownLoad', '/Test', '*.zip', '*.rar', '*.pdb', 'temp*', 'images', 'bootstrap*', 'jquery*', 'artdialog*', 'echarts*', 'kindeditor*', 'layui', 'my97datepic*', 'swfupload*', 'ymprompt*', 'uploadfile*', 'npoi*', 'lenovo*.dll', 'DC.Web.HttpCompress.dll',
+ignore = ['/bin','/AppOffLine', '/*.config', '/*.aspx', '/*.ashx', '/*.asax', '/*.txt', '/Styles', '/DownLoad', '/Test', '*.zip', '*.rar', '*.pdb', 'temp*', 'images', 'bootstrap*', 'jquery*', 'artdialog*', 'echarts*', 'kindeditor*', 'layui', 'my97datepic*', 'swfupload*', 'ymprompt*', 'uploadfile*', 'npoi*', 'lenovo*.dll', 'DC.Web.HttpCompress.dll',
           'DropDownCheckList.dll', 'BouncyCastle.Crypto.dll', 'ComponentArt.Web.UI.dll', 'EntityFramewor*.dll', 'ICSharpCode.SharpZipLib.dll', 'Ionic.Zip.dll', 'ItrusDotNetCertAPI 1.2.dll', 'log4net.DLL', 'Newtonsoft.Json.dll', 'Syste*.dll', 'SAP.Conne*.dll', 'microsoft*', 'QuotationModel', 'ContractModel', '/plugins', '/themes']
 
 # full
