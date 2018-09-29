a9e1187060dd025db3b817f14cd7eaa356d03893
lgy <lgy@qq.com>
2018-09-29 18:52:18 +0800

    1

--- pyscript/1.bat
+++
@@ -1,5 +0,0 @@
-PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer
-
-REM TF workspace /login:lms_tfs16,4567%abc
-
-tf get D:\_work\LMS\SRC-July-Aug /recursive /login:lms_tfs16,4567%%abc
